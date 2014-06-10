fee4c9e3404c4adec0c93ba738f1d5c921c715c8
Alan Li <alan.li@me.com>
2014-06-09 20:59:41 -0400

    clean up.

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS += -std=gnu99
 LOCAL_MODULE    := darm
-LOCAL_SRC_FILES := armv7.c darm.c thumb.c thumb2-decoder.c thumb2.c\
-	darm.h thumb2.h
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_SRC_FILES := armv7.c armv7-tbl.c darm.c darm-tbl.c \
+	thumb.c thumb2-decoder.c thumb2.c thumb-tbl.c thumb2-tbl.c
+#include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
